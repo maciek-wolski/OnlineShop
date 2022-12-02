78016f68e927a7588db7118efd28e25e34113133
Maciej Wolski <maciek.wol16@gmail.com>
2022-12-02 17:41:30 +0100

    Add Entities Added abstract GuidBaseEntity, Product entity and ProductCategory enum

---
+++ ShopAPI/Models/Entity/Common/GuidBaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopAPI.Models.Entity.Common;
+
+public abstract class GuidBaseEntity
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
+    public Guid Id { get; set; }
+    public DateTime CreationDate { get; set; }
+
+    protected GuidBaseEntity()
+    {
+        CreationDate = DateTime.UtcNow;
+    }
+
+    protected GuidBaseEntity(GuidBaseEntity guidBaseEntity) : this()
+    {
+        Id = guidBaseEntity.Id;
+    }
+}
---
+++ ShopAPI/Models/Entity/Product.cs
@@ -0,0 +1,11 @@
+﻿using ShopAPI.Models.Entity.Common;
+using ShopAPI.Models.Entity.Enums;
+
+namespace ShopAPI.Models.Entity;
+
+public class Product : GuidBaseEntity
+{
+    public decimal Price { get; set; }
+    public string? Description { get; set; }
+    public ProductCategory Category { get; set; }
+}
